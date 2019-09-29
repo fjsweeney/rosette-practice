8f588f39cf396707e9d51c76127627f3a99d0461
Forest Sweeney <fjsweeney@users.noreply.github.com>
2019-09-29 15:10:03 -0700

    Comments

--- sudoku-solver.rkt
+++ sudoku-solver.rkt
@@ -1,16 +1,22 @@
 #lang rosette
 
+;; practice constraint programming
+
 (define (int)
   (define-symbolic* i integer?)
+  ;; each sudoku integer is between 1 and 9 inclusive, so we assert that:
   (assert (> i 0))
   (assert (< i 10))
   i)
 
+;; A sudoku puzzle is 9 by 9:
 (define puzzle
   (build-list 9 (thunk*
                  (build-list 9 (thunk*
                                 (int))))))
 
+;; For sudoku, we have constraints on the contents of rows, boxes, and columns
+;; so these are functions that get those groups:
 (define (rows p)
   p)
 
@@ -26,12 +32,8 @@
   (build-list 9 (lambda (a)
                   (build-list 9 (lambda (b)
                                   (get-cell a b p))))))
-(rows puzzle)
-(boxes puzzle)
 
-(define (put x y num)
-  (assert (eq? (list-ref (list-ref puzzle y) x))))
-
+;; let's test the solver on a puzzle:
 (define unsolved
  '((_ _ _ 8 _ 1 _ _ _)
    (_ _ _ _ _ _ 4 3 _)
@@ -43,6 +45,8 @@
    (_ _ 3 4 _ _ _ _ _)
    (_ _ _ 2 _ _ 6 _ _)))
 
+;; For loading the puzzle into our empty template, 
+;; we will just assert that the numerical values need to be in those positions:
 (define (load-puzzle p)
   (build-list 9 (lambda (y)
                   (build-list 9 (lambda (x)
@@ -53,9 +57,14 @@
                                         (#t '_)))))))
 
 (load-puzzle unsolved)
+
+;; Back to our constraints for rows, columns and boxes: there can be no duplicates:
 (define (check-unique list)
   (assert (eq? 9 (length (remove-duplicates list)))))
+  
 (map check-unique (rows puzzle))
 (map check-unique (columns puzzle))
 (map check-unique (boxes puzzle))
+
+;; finally, we ask the computer to find a solution based on those constraints:
 (evaluate puzzle (solve #t))
